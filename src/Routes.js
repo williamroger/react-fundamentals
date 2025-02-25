c1968b6773fa3a7a08f7b0b8019ad703752951b9
William Roger <williamroger.frontend@gmail.com>
2025-02-24 23:03:40 -0300

    adicionada navegação

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Posts from './pages/Posts';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Route path="/" component={Home} />
+    <>
+      <Route exact path="/" component={Home} />
       <Route path="/posts" component={Posts} />
-    </BrowserRouter>
+    </>
   );
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import Header from "../Header";
 import Footer from "../Footer";
 import Routes from '../../Routes';
 
+import { Nav } from './styles';
+
 export default function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Nav>
+        <Link to='/'>Home</Link>
+        <Link to='/posts'>Posts</Link>
+      </Nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
