78584203bb462bef294369980704ced56e893a74
William Roger <williamroger.frontend@gmail.com>
2025-02-25 22:13:21 -0300

    adicionada rota de fallback 404

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Posts from './pages/Posts';
+import NotFound from './pages/NotFound';
 
 export default function Routes() {
   return (
-    <>
+    <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/posts" component={Posts} />
-    </>
+      <Route path="*" component={NotFound} />
+    </Switch>
   );
 }
