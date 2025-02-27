caaa8612e1803be766650057b4bdca856baefc43
William Roger <williamroger.frontend@gmail.com>
2025-02-27 00:07:57 -0300

    adicionado animações nas rotas com react-spring

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 import Home from './pages/Home';
 import Posts from './pages/Posts';
@@ -7,12 +9,21 @@ import Post from './pages/Post';
 import NotFound from './pages/NotFound';
 
 export default function Routes() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/posts" component={Posts} />
-      <Route path="/posts/:id" component={Post} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  );
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: { opacity: 0, transform: 'translateY(50px)', position: 'absolute', },
+    enter: { opacity: 1, transform: 'translateY(0)', position: 'absolute', },
+    leave: { opacity: 0, transform: 'translateY(50px)', position: 'absolute', },
+  });
+
+  return transitions((props, item) => (
+    <animated.div style={props}>
+      <Switch location={item}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Posts} />
+        <Route path="/posts/:id" component={Post} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </animated.div>
+  ));
 }
